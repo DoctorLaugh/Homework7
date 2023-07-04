5ab9c97d17cc045bbaf0bdc7b4fdb10d45d2790c
Dmitriy <trayan@yandex.ru>
2023-07-04 22:21:44 +1100

    Task47 added

---
+++ Task47/Program.cs
@@ -0,0 +1,31 @@
+﻿double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = rnd.Next(minValue, maxValue + 1) + Math.Round(rnd.NextDouble(), 2);
+        }
+    }
+    return result;
+}
+void PrintArray(double[,] inArray)
+{
+    for(int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for(int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+double[,] array = GetArray(rows, columns, -10, 10);
+PrintArray(array);
